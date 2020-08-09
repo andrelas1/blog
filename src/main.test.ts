3208047621fb21c129876dba14c64a64d36ccfff
Andre Luis Araujo Santos <andre.santos0906@gmail.com>
2020-08-09 17:11:10 +0200

    feat: landing page html structure (#19)

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,7 +1,5 @@
-import { test } from "./main";
-
 describe("main", () => {
   it("to pass", () => {
-    expect(test).toBeTruthy();
+    expect(true).toBeTruthy();
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,1 @@
 import "./styles/main.scss";
-
-console.log("IT WORKS!!!!!!");
-
-function testing() {
-  console.log("hello");
-}
-
-export const test = "test";
-
-testing();
