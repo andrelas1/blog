3d8be3d3c09d3a3962ab9b5a4a0249530d712270
Andre Santos <andre.santos0906@gmail.com>
2020-11-14 00:31:34 +0100

    feat(backend): add router.route express example

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,8 @@ app.use(urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 // routes
+
+// TODO: register these two middlewares in the router??
 app.use(
   "/",
   serverStaticsMW(app, "/", [staticsPath]),
@@ -23,12 +25,28 @@ app.use(
   indexRouteController
 );
 
+// TODO: register these two middlewares in the router??
 app.use(
   "/blogpost",
   serverStaticsMW(app, "blogpost", [`${staticsPath}`]),
   templateMW(`${staticsPath}/blogpost/blogpost.ejs`),
   blogpostRouteController
 );
+
+// create API requests with router.route
+const router = express.Router();
+
+router
+  .route("/blog")
+  .get((req, res, next) => {
+    res.send({ test: "test" });
+  })
+  .post((req, res, next) => {
+    console.log("REQ", req.body);
+    res.send({ ok: "ok" });
+  });
+
+app.use("/api", router);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
