63725f4971f59348411ecc0cdb89b9836bc8a6ab
Andre Luis Araujo Santos <andre.santos0906@gmail.com>
2021-03-20 22:18:49 +0100

    feat(blog): md-html converter and improvements to home/blogpost pages (#51)

--- src/resources/blogpost/blogpost.route.ts
+++ src/resources/blogpost/blogpost.route.ts
@@ -1,14 +1,24 @@
 import * as ejs from "ejs";
+import * as showdown from 'showdown';
 
 import { BlogpostModel } from "./blogpost.model";
 
 const r = /(js|css|ttf|png)$/gm;
 
 export const blogpostsRouteController = async (req, res, next) => {
-  console.log("PATGH PORRA", req.params.path);
+  const converter = new showdown.Converter();
+
   const blogpost = await BlogpostModel.findOne({ slug: req.params.path });
+
   if (blogpost && !r.test(req.url)) {
-    ejs.renderFile(res.locals.templatePath, blogpost, (err, template) => {
+    const content = blogpost.get('content');
+    const htmlContent = converter.makeHtml(content);
+    const data = {
+      title: blogpost.get('title'),
+      subtitle: blogpost.get('subtitle'),
+      content: htmlContent
+    };
+    ejs.renderFile(res.locals.templatePath, data, (err, template) => {
       if (err) {
         next(err);
       } else {
