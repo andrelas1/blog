19c64514d0034e92d2e1225f1dec0249032bf73c
Andre Luis Araujo Santos <andre.santos0906@gmail.com>
2021-04-02 12:13:44 +0200

    fix(blog): anchor tags href (#57)
    closes #53

--- src/resources/blogpost/blogpost.route.ts
+++ src/resources/blogpost/blogpost.route.ts
@@ -1,5 +1,5 @@
 import * as ejs from "ejs";
-import * as showdown from 'showdown';
+import * as showdown from "showdown";
 
 import { BlogpostModel } from "./blogpost.model";
 
@@ -11,12 +11,12 @@ export const blogpostsRouteController = async (req, res, next) => {
   const blogpost = await BlogpostModel.findOne({ slug: req.params.path });
 
   if (blogpost && !r.test(req.url)) {
-    const content = blogpost.get('content');
+    const content = blogpost.get("content");
     const htmlContent = converter.makeHtml(content);
     const data = {
-      title: blogpost.get('title'),
-      subtitle: blogpost.get('subtitle'),
-      content: htmlContent
+      title: blogpost.get("title"),
+      subtitle: blogpost.get("subtitle"),
+      content: htmlContent,
     };
     ejs.renderFile(res.locals.templatePath, data, (err, template) => {
       if (err) {
